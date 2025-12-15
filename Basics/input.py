8a9a0b47af6ffd9695a3df2e55f24baa485aa31f
chaitanyakvs <chaitanyakvs76@gmail.com>
2025-12-15 15:07:05 +0530

    Python Basic programs to accept input from user

---
+++ Basics/input.py
@@ -0,0 +1,9 @@
+#Program to accept integer values from user:
+
+#Note: by default the values entered by user at runtime will be treated as string
+
+a=input("Enter Num1: ")
+print(type(a))
+b=input("Enter Num2: ")
+print(type(b))
+print("result is :", a+b)
