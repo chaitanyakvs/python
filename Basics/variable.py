590f4711008c41a16b739411a4da65e5d7929314
chaitanyakvs <chaitanyakvs76@gmail.com>
2025-12-16 14:44:41 +0530

    Python programs on variables

---
+++ Basics/variable.py
@@ -0,0 +1,13 @@
+# Assigning values to Variables in Python
+# assignment operator = to assign a value to a variable.
+# assign value to site_name variable
+site_name = 'python programming'
+print(site_name)
+
+# Changing the Value of a Variable in Python
+number = 10
+print(type(number))
+print(number)
+number = 20
+print(type(number))
+print(number)
