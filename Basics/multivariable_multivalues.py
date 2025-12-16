16b2e50b0f4ab4d4775a28dffabfbefd7a84ecca
chaitanyakvs <chaitanyakvs76@gmail.com>
2025-12-16 16:26:13 +0530

    multiple values to multiple variables and Assigning same value to multiple variable

---
+++ Basics/multivariable_multivalues.py
@@ -0,0 +1,17 @@
+# Assigning multiple values to multiple variables
+
+a, b, c = 5, 9.1, 'Python'
+
+print (a)  # prints 5
+print(type(a))
+print (b)  # prints 9.1
+print(type(b))
+print (c)  # prints python
+print(type(c))
+
+# Assigning same value to the multiple variables
+
+x =  y = 'python.com'
+
+print (x)  # prints python.com
+print (y)  # prints python.com
