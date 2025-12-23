c98ea4888ef1c980b890c85f22ba2ceb2c9adadd
chaitanyakvs <chaitanyakvs76@gmail.com>
2025-12-23 11:47:33 +0530

    function call basic program

---
+++ functions/sample.py
@@ -0,0 +1,11 @@
+
+# Here we are calling a function, we won't get any output if we run the code.
+# Creating a function doesn't mean executing the code inside it. it means the code is there for use # if we want to. To use this function, we need to call the function
+
+def greet():
+    print("Hello World!")
+
+#Calling a function greet()
+
+greet()
+print("outside function")
