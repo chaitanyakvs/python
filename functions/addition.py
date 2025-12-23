d4be72537a537398d9a9df72c8e83b3e5f96adde
chaitanyakvs <chaitanyakvs76@gmail.com>
2025-12-23 12:05:34 +0530

    function with arguments

---
+++ functions/addition.py
@@ -0,0 +1,9 @@
+#function with two arguments
+
+def add_num(num1, num2):
+    sum = num1 + num2
+    print("Sum", sum)
+
+# function call with two values
+
+add_num(5, 7)
---
+++ functions/python_arguments.py
@@ -0,0 +1,14 @@
+# Arguments are inputs given to the function
+
+def greet(name):
+    print("Hello", name)
+
+# Passing Argument
+greet("john")
+
+# Here we passed "john" as an argument to the greet() function
+# We can pass the different arguments in each call, making the function re-usable and dynamic
+
+# Calling a function with a different argument
+
+greet("echo")
