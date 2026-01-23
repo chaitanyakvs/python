80b06f4bb7b35fc8c7e1e90afc75d9ce96e53457
chaitanyakvs <chaitanyakvs76@gmail.com>
2026-01-23 12:11:27 +0530

    Bool() function example

--- Basics/f_string.py
+++ Basics/f_string.py
@@ -12,3 +12,29 @@ print(f" my name is {first_name}")
 # for Integers
 age = 25
 print(f"My age is {age} years")
+
+# for Floating point numbers
+height = 5.9
+print(f"my height is  {height} feet")
+
+# for Boolean
+x = None
+print(bool(x))
+
+# Returns False as x is an empty sequence
+x = 0.0
+print(bool(x))
+
+# Returns False as x is an empty mapping
+x = {}
+print(bool(x))
+
+# Returns True as x is a non empty string
+x = 'GeeksforGeeks'
+print(bool(x))
+
+is_student = True
+if  is_student:
+    print("Yes, I am a student")
+else:
+    print("No, I am not a student")
