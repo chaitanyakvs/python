800117ed3db57f8d9000836b66ec6906da5fc06c
chaitanyakvs <chaitanyakvs76@gmail.com>
2026-01-23 11:38:41 +0530

    f-string example with variable

---
+++ Basics/f_string.py
@@ -0,0 +1,10 @@
+# f-string is nothing but you could use variable along with some text by using f-string
+# always mention the variable in curly braces{variable_name} along with the text
+
+first_name = "chaitanya"
+
+print(first_name)
+
+# By using f-string
+
+print(f" my name is {first_name}")
