36165089b2bb93b75617e3f9513b7d0c98bbff78
chaitanyakvs <chaitanyakvs76@gmail.com>
2026-01-23 11:55:26 +0530

    f-strings along with Integers

--- Basics/f_string.py
+++ Basics/f_string.py
@@ -1,10 +1,14 @@
 # f-string is nothing but you could use variable along with some text by using f-string
 # always mention the variable in curly braces{variable_name} along with the text
 
-first_name = "chaitanya"
+first_name = "chaitanya"        # Here "chaitanya" can be called as string because it is mentioned within quotations
 
 print(first_name)
 
 # By using f-string
 
 print(f" my name is {first_name}")
+
+# for Integers
+age = 25
+print(f"My age is {age} years")
