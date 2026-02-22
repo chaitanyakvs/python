4c37fb3ade0821bbbd65817f604625877d5d37d6
chaitanyakvs <chaitanyakvs76@gmail.com>
2026-02-22 12:30:54 +0530

    Typecasting example in Python

---
+++ Basics/typecating.py
@@ -0,0 +1,14 @@
+# Type casting is the process of converting a value from one type to another. In Python, you can use built-in functions to perform type casting.
+
+# Example of type casting:
+
+name = "sample text"
+age = 25
+gpa = 3.5
+is_student = True
+print(type(is_student))
+gpa = int(gpa)
+print(gpa)
+print(type(gpa))
+# In this example, we have a string variable `name`, an integer variable `age`, a float variable `gpa`, 
+# and a boolean variable `is_student`. We then convert the float `gpa` to an integer using the `int()` function. After the conversion, we print the new value of `gpa` and its type to confirm the change.
