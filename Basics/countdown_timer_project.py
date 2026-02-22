bd55d754ced9593842476519a2677c7dcf3a179c
chaitanyakvs <chaitanyakvs76@gmail.com>
2026-02-22 15:39:18 +0530

    Mini project using for loop and importing module

---
+++ Basics/countdown_timer_project.py
@@ -0,0 +1,9 @@
+# Countdown timer project using import modules and for loop
+
+import time
+
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)  # Pause the program for 1 second 
+
+print("Time's Up!")
